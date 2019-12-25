c5acdf64749f2b123154e03f187fdc78d9c10f65
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-25 13:14:31 +0300

    Created project

---
+++ DesignPatterns_Flyweight/DesignPatterns_Flyweight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Flyweight");
+        }
+    }
+}
